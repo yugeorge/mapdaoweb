10a762caf1fc12cbcb65140166703c2a0563614a
yugeorge <yugeorge@yeah.net>
2014-11-26 14:26:15 +0800

    17th

--- cloud/app.js
+++ cloud/app.js
@@ -16,7 +16,7 @@ app.get('/hello', function(req, res) {
   res.render('hello', { message: 'Congrats, you just set up your app!' });
 });
 
-app.get('/index',function(req, res){
+app.get('/',function(req, res){
    res.render('index',{ message:'you are the best'});
 });
 
@@ -28,15 +28,15 @@ app.get('/edit',function(req, res){
     res.render('edit',{ message:'you are yourself'});
 });
 
-app.post('/index', function(req, res) {
+app.post('/', function(req, res) {
     AV.User.logIn(req.body.username, req.body.password).then(function() {
       //登录成功，avosExpressCookieSession会自动将登录用户信息存储到cookie
       //跳转到profile页面。
-      alert('signin successfully: %j', AV.User.current());
+      console.log('signin successfully: %j', AV.User.current());
       res.redirect('/edit');
     },function(error) {
       //登录失败，跳转到登录页面
-      res.redirect('/index');
+      res.redirect('/');
   });
 });
 
