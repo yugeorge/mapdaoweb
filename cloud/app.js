1474530b3438e0c0847465e14044eb9d1fd8a5d0
yugeorge <yugeorge@yeah.net>
2014-11-26 11:36:46 +0800

    seventh

--- cloud/app.js
+++ cloud/app.js
@@ -7,6 +7,10 @@ app.set('views','cloud/views');   // 设置模板目录
 app.set('view engine', 'ejs');    // 设置 template 引擎
 app.use(express.bodyParser());    // 读取请求 body 的中间件
 
+app.use(express.cookieParser('Your Cookie Secure'));
+//使用avos-express-cookie-session记录登录信息到cookie。
+app.use(avosExpressCookieSession({ cookie: { maxAge: 3600000 }}));
+
 // 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
 app.get('/hello', function(req, res) {
   res.render('hello', { message: 'Congrats, you just set up your app!' });
@@ -23,6 +27,21 @@ app.get('/register',function(req, res){
 app.get('/edit',function(req, res){
     res.render('edit',{ message:'you are yourself'});
 });
+
+app.post('/index', function(req, res) {
+    AV.User.logIn(req.body.username, req.body.password).then(function() {
+      //登录成功，avosExpressCookieSession会自动将登录用户信息存储到cookie
+      //跳转到profile页面。
+      console.log('signin successfully: %j', AV.User.current());
+      res.redirect('/edit');
+    },function(error) {
+      //登录失败，跳转到登录页面
+      res.redirect('/index');
+  });
+});
+
+
+
 
 // 最后，必须有这行代码来使 express 响应 HTTP 请求
 app.listen();
