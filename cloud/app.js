3e738f9db72f9933f43f0090d18b741dfbb2152b
yugeorge <yugeorge@yeah.net>
2014-11-28 17:28:10 +0800

    27th

--- cloud/app.js
+++ cloud/app.js
@@ -1,6 +1,7 @@
 // 在 Cloud code 里初始化 Express 框架
 var express = require('express');
 var app = express();
+var fs = require('fs');
 
 // App 全局配置
 app.set('views','cloud/views');   // 设置模板目录
@@ -36,12 +37,8 @@ app.post('/register',function(req, res){
   error: function(user, error) {
     // Show the error message somewhere and let the user try again.
     res.render('register',{ message:error.message});
-	
-  
 }}
-
 );
-
 });
 
 app.get('/edit',function(req, res){
@@ -62,9 +59,27 @@ app.post('/', function(req, res) {
   });
 });
 
-//app.post('/register', function(req, res){
-  //   AV.User.signUp(
-//)
+app.post('/edit',function(req,res){
+     var iconFile = req.files.exampleInputFileA;
+  if(iconFile){
+    fs.readFile(iconFile.path, function(err, data){
+      if(err)
+        return res.send("读取文件失败");
+      var base64Data = data.toString('base64');
+      var theFile = new AV.File(iconFile.name, {base64: base64Data});
+      theFile.save().then(function(theFile){
+        res.send("上传成功！");
+      });
+    });
+  }else
+    res.send("请选择一个文件。");
+
+
+
+
+});
+
+
 
 
 
