5603ff1c7b8c9360ff79a9d73b26fe2ad01ceeb0
yugeorge <yugeorge@yeah.net>
2014-11-28 20:54:17 +0800

    30th

--- cloud/app.js
+++ cloud/app.js
@@ -60,17 +60,19 @@ app.post('/', function(req, res) {
 });
 
 app.post('/edit',function(req,res){
-   var fileUploadControl = $("#exampleInputFileA")[0];
-if (fileUploadControl.files.length > 0) {
-  var file = fileUploadControl.files[0];
-  var name = "photo.jpg";
-  var avFile = new AV.File(name, file);
-}
-  avFile.save().then(function() {
-  // The file has been saved to AV.
-}, function(error) {
-  // The file either could not be read, or could not be saved to AV.
-});
+ var iconFile = req.files.example;
+  if(iconFile){
+    fs.readFile(iconFile.path, function(err, data){
+      if(err)
+        return res.send("读取文件失败");
+      var base64Data = data.toString('base64');
+      var theFile = new AV.File(iconFile.name, {base64: base64Data});
+      theFile.save().then(function(theFile){
+        res.send("上传成功！");
+      });
+    });
+  }else
+    res.send("请选择一个文件。");
 
 
 
